d6926d2d36bd1b33d1af4d4aa148a366c67d71bd
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-09 12:26:14 -0700

    Create initial customers routes

---
+++ routes/customers-api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const customerQueries = require('../db/queries/customers');
+
+router.get('/', (req, res) => {
+  customerQueries.getCustomers()
+    .then(customers => {
+      res.json({ customers });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+//Get customer/:id
+router.get('/:id', (req, res) => {
+  const customerID = req.params.id;
+  customerQueries.getCustomerByID(customerID)
+    .then(customer => {
+      res.json({ customer });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+module.exports = router;
