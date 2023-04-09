ea3ec37537ce9defcd95274a27899c9d5f0be2db
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-09 11:10:47 -0600

    Fix the spelling errors in the seed file

--- db/seeds/05_order_items.sql
+++ db/seeds/05_order_items.sql
@@ -1,4 +1,11 @@
-INSERT INTO order_items (order_id, menu_item_id, quantity)
-VALUES (1, 1, 1),
-(1, 2, 3),
-(1, 4, 5);
+INSERT INTO
+  order_items (order_id, menu_item_id, quantity)
+VALUES
+  (1, 1, 1),
+  (1, 2, 3),
+  (1, 4, 5),
+  (2, 6, 2),
+  (2, 7, 4),
+  (3, 8, 3),
+  (3, 9, 1),
+  (3, 10, 9);
