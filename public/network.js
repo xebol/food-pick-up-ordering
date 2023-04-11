87d963a4569bc0f8bf2fced7f7b966dd3335af24
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-11 13:33:14 -0700

    Add checkLogIn function

--- public/network.js
+++ public/network.js
@@ -1,5 +1,13 @@
+function checkLogIn() {
+  console.log("Welcome to Luigi's!");
+  return $.ajax({
+    method: 'GET',
+    url: "/api/customers/me",
+  });
+}
+
 const logOut = () => {
-  console.log('Signing out...')
+  console.log('Signing out...');
   return $.ajax({
     method: 'POST',
     url: '/api/logout'
