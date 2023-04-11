e3a42136a5c08f3ae69ca98f1b81828f5f7e8783
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-11 10:17:47 -0700

    Utilize logOut helper function

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,15 +3,13 @@ $(document).ready(function() {
   console.log('Document is ready to use jQUery');
 
   $("#logout").on('click', function() {
-    $.ajax({
-      method: 'POST',
-      url: '/api/logout'
-    })
+    logOut()
       .then(() => {
         console.log('You have successfully logged out');
       })
       .catch((err) => {
         console.log(err);
       });
+
   });
 });
