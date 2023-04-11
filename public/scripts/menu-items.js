ff2b029becd5f344682f784f2718723dd62aa647
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-11 16:33:58 -0600

    Moving the cart code over to menu-items branch

---
+++ public/scripts/menu-items.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+
+  $('.menu-items').on('click', () => {
+    $.ajax({
+      method: 'GET',
+      url: '/api/menu_items'
+    })
+      .done((response) => {
+        console.log(response)
+        const $orderList = $('.order-items');
+        $orderList.empty();
+
+        $orderList.append(`<h2>${response.orders[0].id}</h2>`);
+
+      //   for (const order of response.orders) {
+      //     $orderList.append(`<h2>${order.id}</h2>`);
+      //   }
+      });
+  });
+});
