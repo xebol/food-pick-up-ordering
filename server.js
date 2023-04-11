82007d7cf047460a1871a1e3e41f61281517e63f
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-10 18:05:15 -0700

    Removed body-parser redundancy

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
