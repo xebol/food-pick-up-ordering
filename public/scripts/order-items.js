a5617df59d6df011becabe4d2c268f24e5829a5c
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-10 19:59:47 -0600

    Test new branch

---
+++ public/scripts/order-items.js
@@ -0,0 +1,16 @@
+//   <script>
+//   $(document).ready (function (){
+//     console.log('loaded');
+//     $('.add').click(function() {
+//       console.log('Added to Cart')
+//       $(this).attr('data-name')
+//       console.log('data-name', $(this).attr('data-name'))
+//     })
+//   })
+// {/* </script> */}
+
+$(document).ready(function() {
+  $('.icon').click(function() {
+    console.log('You clicked on the cart')
+  })
+});
