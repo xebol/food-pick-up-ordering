a8b61e3c7388ce284441472da47b70f8813d7492
Kian <kiannaimiroy@gmail.com>
2023-04-12 16:55:39 -0700

    implemented Twilio

--- send_sms.js
+++ send_sms.js
@@ -1,13 +1,15 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = 'ACed1ee8ee5825b6a4ba593a96f17b7d41';
-const authToken = '42a3593ec68df81c48eabe4d32b9b011';
+require('dotenv').config()
+
+const accountSid = process.env.DB_ACC_SID;
+const authToken = process.env.DB_AUTH_KEY;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     body: 'DYNAMIC CODE HERE FOR ORDER',
      from: '+15075167661',
      to: '+15148338334'
    })
