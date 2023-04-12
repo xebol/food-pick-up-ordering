2286d48873fcdb128cf6007cadcd380cefbbc658
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-12 17:53:50 -0600

    Add items to cart and remove items from cart. Thanks Rohit

--- public/scripts/place-order.js
+++ public/scripts/place-order.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   console.log('Document is ready: The Sequel');
+  //clear the browser storage
+  localStorage.clear();
 
   const placeOrder = function(item) {
     const $orderId = $(`
-    <div class="order-item">
+    <div class="order-item" id="removeItem">
           <h2>${item.name}</h2>
           <div class="order-item-p">
             <p>-</p>
             <p>1</p>
             <p>+</p>
           </div>
-          <i class="fa-solid fa-trash-can"></i>
+          <i id="${item.id}" class="fa-solid fa-trash-can"></i>
         </div>
         `);
     return $orderId;
@@ -18,58 +20,38 @@ $(document).ready(function() {
 
   const renderItem = function(item) {
     const $menuItems = $('.order-items');
-    $menuItems.empty();
-    //loop through the orders
     let $orderItem = placeOrder(item);
     $menuItems.append($orderItem);
 
   };
 
   $('.items').on('click', '.icon', function() {
-    console.log(this);
-    let menuItemID;
-    if ($(this).is('#1')) {
-      menuItemID = 1;
-    }
-    if ($(this).is('#2')) {
-      menuItemID = 2;
-    }
-    if ($(this).is('#3')) {
-      menuItemID = 3;
-    }
-    if ($(this).is('#4')) {
-      menuItemID = 4;
-    }
-    if ($(this).is('#5')) {
-      menuItemID = 5;
-    }
-    if ($(this).is('#6')) {
-      menuItemID = 6;
-    }
-    if ($(this).is('#7')) {
-      menuItemID = 7;
-    }
-    if ($(this).is('#8')) {
-      menuItemID = 8;
-    }
-    if ($(this).is('#9')) {
-      menuItemID = 9;
-    }
-    if ($(this).is('#10')) {
-      menuItemID = 10;
-    }
-    console.log('Menu Item ID: ', menuItemID);
+    let menuItemID = $(this).attr('id');
+
     $.ajax({
       method: 'GET',
       url: `/api/menu_items/${menuItemID}`
     })
       .then((items) => {
         console.log('AJAX response: ', items);
+        //to store an data to the browser storage, turn it into a string
+        localStorage.setItem(`${menuItemID}`, JSON.stringify(items));
+        //To view the the browser storage
+        // console.log('localStorage', localStorage);
+        // const retrievedObject = JSON.parse(localStorage.getItem(`${menuItemID}`));
+        // console.log('retrievedObject', retrievedObject);
         renderItem(items.item);
       });
 
   });
+
+  $('.cart').on('click', '.fa-trash-can', function() {
+
+    let temp = $(this).attr("id");
+    console.log("trash can id ", temp);
+    $(this).parent().empty();
+  });
+
 });
 
-// When we push a particular button, we want the corresponding menu item to appear in the cart.
-//Button id matches menu_items id.
+
