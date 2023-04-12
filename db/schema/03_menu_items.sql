39147c4a0fe0238d15bb83e359a6576cf23aedaa
Kian <kiannaimiroy@gmail.com>
2023-04-11 21:20:04 -0700

    add menu items to dynamic html

--- db/schema/03_menu_items.sql
+++ db/schema/03_menu_items.sql
@@ -4,6 +4,6 @@ CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
-  foodImageName TEXT NOT NULL,
+  food_image_name TEXT NOT NULL,
   price INTEGER NOT NULL
 );
--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,11 +1,11 @@
-INSERT INTO menu_items (name, description, price)
+INSERT INTO menu_items (name, description, food_image_name, price)
 VALUES('Luigi''s Linguini', 'Fresh-made pasta in a Goomba cream sauce', '/images/linguine.png', 20000),
-('Slippy Banana Splitty', 'Ripe banana with vanilla ice cream and chocolate from the mountain', 15000),
-('Toad''s Taters', 'Fried to crispy golden perfection', 14000),
-('Bowser Burger', 'Makes you breathe fire like King Koopa', 24000),
-('Baby Bowser Slider', 'A little less spice than the Bowser burger, but just as tasty!', 17000),
-('Daisie''s Pancake Stack', 'Covered in whipped cream and golden syrup', 7800),
-('Dry Bone''s Marrow', 'Parsley, capers, shallots', 40000),
-('Stuffed Piranha Plant', 'Stuffed with fresh mushrooms and grilled until perfect', 34000),
-('The Shy Guy Special', 'Two eggs over-easy, hash browns, rye toast all covered in hollandaise', 15000),
-('Item Box', 'Chef Luigi Mario cooks for you', 50000);
+('Slippy Banana Splitty', 'Ripe banana with vanilla ice cream and chocolate from the mountain', '/images/banana-split.png', 15000),
+('Toad''s Taters', 'Fried to crispy golden perfection', '/images/tater-tots.png', 14000),
+('Bowser Burger', 'Makes you breathe fire like King Koopa', '/images/bowser-burger.png', 24000),
+('Baby Bowser Slider', 'A little less spice than the Bowser burger, but just as tasty!', '/images/baby-burger.png', 17000),
+('Daisie''s Pancake Stack', 'Covered in whipped cream and golden syrup', '/images/pancake-png.jpeg', 7800),
+('Dry Bone''s Marrow', 'Parsley, capers, shallots', '/images/bone-marrow.png', 40000),
+('Stuffed Piranha Plant', 'Stuffed with fresh mushrooms and grilled until perfect', '/images/piranha-plant.png', 34000),
+('The Shy Guy Special', 'Two eggs over-easy, hash browns, rye toast all covered in hollandaise', '/images/breakfast.png', 15000),
+('Item Box', 'Chef Luigi Mario cooks for you', '/images/chef.png', 50000);
