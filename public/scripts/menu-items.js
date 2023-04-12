233d60720c60ae9ac8b54ac4b7fc89ffd6170509
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-11 18:45:49 -0600

    Able to render data from database

--- public/scripts/menu-items.js
+++ public/scripts/menu-items.js
@@ -1,21 +1,18 @@
 
 $(document).ready(function() {
 
-  $('.menu-items').on('click', () => {
+  $('.icon').on('click', () => {
     $.ajax({
       method: 'GET',
       url: '/api/menu_items'
     })
       .done((response) => {
-        console.log(response)
+        console.log('response', response)
         const $orderList = $('.order-items')
         $orderList.empty();
 
-        $orderList.append(`<h2>${response.orders[0].id}</h2>`);
-
-      //   for (const order of response.orders) {
-      //     $orderList.append(`<h2>${order.id}</h2>`);
-      //   }
+        $orderList.append(`<h2>${response.items[2].name}</h2>`)
       });
+
   });
 });
