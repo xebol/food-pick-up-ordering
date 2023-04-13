825a67bbb5fa864f6f0c64eaf03cba045d2aff0c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-12 23:55:07 -0700

    Alter layout for admin

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -11,69 +11,43 @@ $(document).ready(function() {
       navLinks = `
       <span class="logo"><img src="/images/luigichef.webp" alt="Image" width="110px" height="110px"></span>
       <div class="nav-items">
-        <p><a href="#header">About</a></p>
-        <p><a href="#menu">Menu</a></p>
-        <p><a href="#reviews">Reviews</a></p>
-        <p id="login">Login</p>
+      <p><a href="#header">About</a></p>
+      <p><a href="#menu">Menu</a></p>
+      <p><a href="#reviews">Reviews</a></p>
+      <p id="login">Login</p>
       </div>
       `;
+      console.log('Please sign in');
+    } else if (user.admin === true) {
+      navLinks = `
+      <span class="logo"><img src="/images/luigichef.webp" alt="Image" width="110px" height="110px"></span>
+      <div class="nav-items">
+      <p id="logout">Logout</p>
+      </div>
+      `;
+      console.log('USER:', user.admin);
     } else {
       navLinks = `
       <span class="logo"><img src="/images/luigichef.webp" alt="Image" width="110px" height="110px"></span>
       <div class="nav-items">
-        <p><a href="#header">About</a></p>
-        <p><a href="#menu">Menu</a></p>
-        <p><a href="#reviews">Reviews</a></p>
-        <p id="logout">Logout</p>
-        <p><i class="fa-solid fa-cart-shopping"></i></p>
+      <p><a href="#header">About</a></p>
+      <p><a href="#menu">Menu</a></p>
+      <p><a href="#reviews">Reviews</a></p>
+      <p id="logout">Logout</p>
+      <p><i class="fa-solid fa-cart-shopping"></i></p>
       </div>
       `;
+      console.log('USER:', user.admin);
     }
 
     $pageNav.append(navLinks);
+
 
   }
 
   checkLogIn()
     .then(function(json) {
       updateNav(json.user);
-    });
-
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
-        }
-      }
     });
 
   $('#nav-bar').on('click', '#logout', function() {
