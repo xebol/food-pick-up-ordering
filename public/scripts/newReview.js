d82f9071238e33cb96d5f5b829235a9cb2560e83
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-04-12 18:40:52 -0700

    Move functions for readability, edit date variable to make timeago work properly, fix jQuery selector so textarea clears after submitting a review

--- public/scripts/newReview.js
+++ public/scripts/newReview.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
-  //hiding error messages
-  // $('#error-message').hide();
+
+  //a helper function to make calling errors easier.
+  // const displayError = function(message) {
+  //   $('#error-message').html(message);
+  //   return $('#error-message').slideDown(400);
+  // };
+  //makes a review object that is then passed to the database
+  const createReviewElement = function(review) {
+
+    console.log("createReviewElement", review);
+    //timestamps
+    const timeAgo = timeago.format(review.date);
+
+    //escapes the user input for security
+    function escape(string) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(string));
+      return div.innerHTML;
+    };
+
+    const dynamicReview = `<div class="review">
+        <h3>${escape(review.name)}</h3>
+        <p>${escape(review.message)}</p>
+        <p>${timeAgo}</p>
+        </div>`;
+    return dynamicReview;
+
+  };
 
   const renderReviews = function(reviews) {
     $('.reviews').empty();
@@ -19,47 +45,18 @@ $(document).ready(function() {
       method: 'GET',
       url: '/api/reviews',
     }).then((reviews) => {
-      console.log("reviews from db", reviews)
+      console.log("reviews from db", reviews);
       renderReviews(reviews);
     });
   };
+
   loadReviews();
-
-  //a helper function to make calling errors easier.
-  // const displayError = function(message) {
-  //   $('#error-message').html(message);
-  //   return $('#error-message').slideDown(400);
-  // };
-  //makes a review object that is then passed to the database
-  const createReviewElement = function(review) {
-
-  console.log("REVIEW", review)
-    //timestamps
-    const timeAgo = timeago.format(review.created_at);
-
-    //escapes the user input for security
-    function escape(string) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(string));
-      return div.innerHTML;
-    };
-
-    const dynamicReview =  `<div class="review">
-      <h3>${escape(review.name)}</h3>
-      <p>${escape(review.message)}</p>
-      <p>${timeAgo}</p>
-      </div>`
-      return dynamicReview;
-
-    };
 
   //submit button logic
   $('#review-form').on('submit', (event) => {
-    // $('#review-form').val('');
-  // console.log("BUTTON TEST")
-    //stops the browser sunning default operations
+    //stops the browser running default operations
     event.preventDefault();
-    //error message triggers
+    // error message triggers
     // const $reviewLength = $('textarea').val().length;
 
     // if ($reviewLength <= 0) {
@@ -67,21 +64,18 @@ $(document).ready(function() {
     // }
     //turns encoded url into a serialized string
     const encodedURL = $('#review-form').serialize();
-    console.log("encodedURL", encodedURL)
-    $('#review-text').val('');
-      //ajax method to actually post the created tweet after the button has been pressed
-      $.ajax({
-        method: 'POST',
-        url: '/api/reviews',
-        data: encodedURL,
-      }).then((response) => {
-        console.log("TEST", response)
-        loadReviews();
-        // $('#error-message').slideUp(400);
-      }).catch((err) => {
-        console.log('ERROR')
-        // return displayError("Something went wrong! please try again.");
-      });
+    $('.review-text').val('');
+    //ajax method to actually post the created tweet after the button has been pressed
+    $.ajax({
+      method: 'POST',
+      url: '/api/reviews',
+      data: encodedURL,
+    }).then((response) => {
+      console.log('Successfully added review: ', response);
+      loadReviews();
+    }).catch((err) => {
+      console.log(err);
+    });
   });
 
 });
