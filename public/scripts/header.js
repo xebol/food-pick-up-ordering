34fd55c28df6a34662f28eef2656b8f89e1865cc
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-13 19:58:43 -0600

    Add admin page

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -4,17 +4,33 @@ $(document).ready(function() {
 
   const $pageHeader = $('#header');
   function updateHeader(user) {
-    console.log('Updating header...')
+    console.log('Updating header...');
     let headerMessage;
 
-    if (user.admin === true) {
+    if (!user) {
+      headerMessage = `
+      <h1 id="luigis">Luigi's</h1>
+      <h6 id="about-us-header">About Us</h6>
+      <p id="desc" style="text-align: justify">
+
+        Luigi's restaurant in the Mushroom Kingdom is a delightful dining spot that
+        offers a unique experience to its visitors. The restaurant is the latest venture of Luigi,
+        a beloved figure in the kingdom, known for his skills as a plumber, doctor, ghost hunter, racecar driver,
+        pro tennis player and MUCH more.
+        <br>
+        <br>
+        Now, Luigi brings his love for exploration and his passion for quality food to his restaurant.
+        The menu features dishes made with locally-sourced ingredients that highlight the
+        flavors of the Mushroom Kingdom. From classic linguini to innovative creations like Dry Bone's Marrow,
+        the dishes are expertly crafted and bursting with flavors.
+      </p>`;
+    } else if (user.admin) {
       headerMessage = `
       <h1 id="luigis-admin">Welcome Back, Chef!</h1>
       <h2 class="orders">Current Orders</h2>
       <article class="admin-order-template">
 
-      </article>
-      `;
+      </article>`;
     } else {
       headerMessage = `
       <h1 id="luigis">Luigi's</h1>
@@ -31,8 +47,7 @@ $(document).ready(function() {
         The menu features dishes made with locally-sourced ingredients that highlight the
         flavors of the Mushroom Kingdom. From classic linguini to innovative creations like Dry Bone's Marrow,
         the dishes are expertly crafted and bursting with flavors.
-      </p>
-      `;
+      </p>`;
     }
 
     $pageHeader.append(headerMessage);
